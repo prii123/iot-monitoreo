2767ade1c09714bcb704dc4be3181d599d61ac32
prii123 <printsvallejos@gmail.com>
2025-04-17 21:51:16 -0500

    .

--- iot_front/src/utils/api.js
+++ iot_front/src/utils/api.js
@@ -1,4 +1,4 @@
 // utils/api.js
-const API_BASE_URL = 'http://localhost:4000/api'; // Cambia si usas Docker o producción
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL; // Cambia si usas Docker o producción
 
 export default API_BASE_URL;
